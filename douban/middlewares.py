bddbbcacbabcbd0c34b4cc524793bd389fa09a5b
capricorncd <1355952333@qq.com>
2020-05-31 10:25:46 +0900

    Update userAgent

--- douban/middlewares.py
+++ douban/middlewares.py
@@ -114,23 +114,34 @@ class CamouflageIP(object):
         request.headers['Proxy-Authorization'] = 'Basic ' + encode_name_password.decode()
 
 
+# user agent 列表
+USER_AGENT_LIST = [
+    'MSIE (MSIE 6.0; X11; Linux; i686) Opera 7.23',
+    'Opera/9.20 (Macintosh; Intel Mac OS X; U; en)',
+    'Opera/9.0 (Macintosh; PPC Mac OS X; U; en)',
+    'iTunes/9.0.3 (Macintosh; U; Intel Mac OS X 10_6_2; en-ca)',
+    'Mozilla/4.76 [en_jp] (X11; U; SunOS 5.8 sun4u)',
+    'iTunes/4.2 (Macintosh; U; PPC Mac OS X 10.2)',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0) Gecko/20100101 Firefox/5.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:9.0) Gecko/20100101 Firefox/9.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20120813 Firefox/16.0',
+    'Mozilla/4.77 [en] (X11; I; IRIX;64 6.5 IP30)',
+    'Mozilla/4.8 [en] (X11; U; SunOS; 5.7 sun4u)',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; InfoPath.3; Shuame; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; InfoPath.3; Shuame; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; InfoPath.3; Shuame; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; InfoPath.3; Shuame; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
+]
+
+
 # camouflage userAgent
 class CamouflageUserAgent(object):
     def process_request(self, request, spider):
-        # user agent 列表
-        USER_AGENT_LIST = [
-            'MSIE (MSIE 6.0; X11; Linux; i686) Opera 7.23',
-            'Opera/9.20 (Macintosh; Intel Mac OS X; U; en)',
-            'Opera/9.0 (Macintosh; PPC Mac OS X; U; en)',
-            'iTunes/9.0.3 (Macintosh; U; Intel Mac OS X 10_6_2; en-ca)',
-            'Mozilla/4.76 [en_jp] (X11; U; SunOS 5.8 sun4u)',
-            'iTunes/4.2 (Macintosh; U; PPC Mac OS X 10.2)',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0) Gecko/20100101 Firefox/5.0',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:9.0) Gecko/20100101 Firefox/9.0',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20120813 Firefox/16.0',
-            'Mozilla/4.77 [en] (X11; I; IRIX;64 6.5 IP30)',
-            'Mozilla/4.8 [en] (X11; U; SunOS; 5.7 sun4u)'
-        ]
         # 随机生成user agent
         user_agent = random.choice(USER_AGENT_LIST)
         request.headers['User_Agent'] = user_agent
